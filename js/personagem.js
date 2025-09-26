c69f0e34849a46aaffb21970846e11ad79963a71
ELOA FONSECA SANTOS <eloa.fsantos@senacsp.edu.br>
2025-09-25 21:58:08 -0300

    fiz o javascript

---
+++ js/personagem.js
@@ -0,0 +1,55 @@
+const txtNome = document.querySelector('#personagem')
+const txtPai = document.querySelector('#pai');
+const txtMae = document.querySelector('#mae');
+const txtVila = document.querySelector('#vila');
+const txtRank = document.querySelector('#rank');
+const txtResumo = document.querySelector('#resumo');
+const txtPower = document.querySelector('#power');
+const txtJutsus = document.querySelector('#jutsus');
+
+async function buscaPersonagem() {
+
+
+    const resultDiv = document.querySelector('#resultDiv');
+    let personagemBuscado = document.querySelector('#personagem').value;
+
+
+    await fetch(`https://naruto-br-api.site/characters/${personagemBuscado}`)
+        .then(response => response.json())
+        .then(dados => {
+
+            console.log(dados);
+
+            if (!dados.name) {
+                resultDiv.innerHTML = '<p>Personagem não encontrado. Tente novamente.</p>';
+
+            } else {
+
+
+                txtNome.value = dados.name || "Nome não informado";
+                txtPai.value = dados.family?.father || "Desconhecido";
+                txtMae.value = dados.family?.mother || "Desconhecida";
+                txtVila.value = dados.village || "Não informado";
+                txtRank.value = dados.rank || " Não informado";
+                txtResumo.value = dados.info || "Sem resumo disponível";
+                txtPower.value = dados.power || "Não informado";
+                txtJutsus.value = (dados.jutsus && dados.jutsus.length > 0) 
+                ? dados.jutsus.join(", ") 
+                : "Nenhum jutsu cadastrado";
+            }
+
+
+        })
+        .catch(err =>{
+            console.error(err);
+            resultDiv.innerHTML = '<p>Erro ao buscar personagem.</p>'
+
+        });
+
+
+
+}
+
+function limparDados(){
+    txtNome
+}
